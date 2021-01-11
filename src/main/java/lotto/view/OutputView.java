3bacf66c0b7439a3becca48d9ae8b26aea63f3fd
Sangyeol Kim <syeol.kim@daum.net>
2021-01-11 18:50:57 +0900

    refactor: 출력 함수 수정
    원시값 포장과 매칭 결과의 전달 방식 변경에 따라 함수를 수정함.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,58 +5,40 @@ import lotto.domain.*;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    public static void printLottoTicketNum(int investment) {
-        int numTickets = investment / LottoTicketIssuer.TICKET_PRICE;
-        System.out.println(numTickets + "개를 구매했습니다.");
+    public static void printNumberOfLottoPurchased(Price inputPrice) {
+        int number = inputPrice.divide(LottoTicketIssuer.TICKET_PRICE).getPrice();
+        System.out.println(number + "개를 구매했습니다.");
     }
 
-    public static void printLottoTickets(LottoTickets lottoTickets) {
-        StringBuilder message = new StringBuilder();
-
-        lottoTickets.toStream()
-                .forEach(ticket -> {
-                    LottoNumbers lottoNumbers = ticket.getLottoNumbers();
-                    String numbers = lottoNumbers.toStream()
-                            .map(LottoNumber::getNumber)
-                            .map(num -> Integer.toString(num))
-                            .collect(Collectors.joining(", "));
-
-                    message.append("[")
-                            .append(numbers)
-                            .append("]")
-                            .append(System.lineSeparator());
-                });
-
-        System.out.println(message);
+    public static void printLottoTicket(LottoTicket lottoTicket) {
+        String message = lottoTicket.getLottoNumbersList()
+                .stream()
+                .map(LottoNumbers::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(message + System.lineSeparator());
     }
 
-    public static void printStatistics(LottoStatistics statistics) {
+    public static void printStatistics(MatchResult matchResult, Rate rateOfReturn) {
         StringBuilder message = new StringBuilder();
 
         message.append("당첨 통계")
                 .append(System.lineSeparator())
-                .append("---------\n");
+                .append("---------")
+                .append(System.lineSeparator());
 
-        StatisticsResult statisticsResult = statistics.getStatisticsResult();
-        MatchResults matchResults = statisticsResult.getMatchResults();
-        Rate earningRate = statisticsResult.getEarningRate();
+        for (Rank rank : Rank.values()) {
+            message.append(rank.getMatchCount())
+                    .append("개 일치 (")
+                    .append(rank.getPrize())
+                    .append("원) - ")
+                    .append(matchResult.getCount(rank))
+                    .append("개")
+                    .append(System.lineSeparator());
+        }
 
-        matchResults.getResultCounts()
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    MatchResult result = entry.getKey();
-                    int count = entry.getValue();
-
-                    return String.format("%d개 일치 (%d원) - %d개\n",
-                            result.getMatchCount(),
-                            result.getReward(),
-                            count);
-                })
-                .forEach(message::append);
-
-        message.append(
-                String.format("총 수익률은 %d%%입니다.", earningRate.getRate()));
+        message.append("총 수익률은 ")
+                .append(rateOfReturn.getRate())
+                .append("% 입니다.");
 
         System.out.println(message);
     }
