bd5559ee7f1059f6aeabd8e333b8799e381d161a
Sangyeol Kim <syeol.kim@daum.net>
2021-01-11 18:28:36 +0900

    refactor: LottoNumbers를 랜덤하게 생성하는 기능 추가

--- src/main/java/lotto/domain/LottoTicketIssuer.java
+++ src/main/java/lotto/domain/LottoTicketIssuer.java
@@ -1,19 +1,36 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public final class LottoTicketIssuer {
-    public static final int TICKET_PRICE = 1000;
+    public static final Price TICKET_PRICE = new Price(1000);
+    private static final List<LottoNumber> numberList = IntStream.rangeClosed(
+            LottoNumber.LOWER_BOUND, LottoNumber.UPPER_BOUND)
+            .mapToObj(LottoNumber::new)
+            .collect(Collectors.toList());
 
-    private LottoTicketIssuer() {}
+    private LottoTicketIssuer() {
+    }
 
-    public static LottoTickets issue(int insertPrice) {
-        List<LottoTicket> tickets = IntStream.range(0, insertPrice / TICKET_PRICE)
-                .boxed()
-                .map(x -> new LottoTicket())
+    public static LottoTicket issue(Price inputPrice) {
+        List<LottoNumbers> numbersList =
+                IntStream.range(0,
+                        inputPrice.divide(TICKET_PRICE)
+                                .getPrice()
+                )
+                        .mapToObj(x -> createLottoNumbersRandomly())
+                        .collect(Collectors.toList());
+        return new LottoTicket(numbersList);
+    }
+
+    private static LottoNumbers createLottoNumbersRandomly() {
+        Collections.shuffle(numberList);
+        List<LottoNumber> numbers = numberList.stream()
+                .limit(LottoNumbers.REQUIRED_SIZE)
                 .collect(Collectors.toList());
-        return new LottoTickets(tickets);
+        return new LottoNumbers(numbers);
     }
 }
