48183e8a24fda9dc957763f73595fc66a452b42c
Sangyeol Kim <syeol.kim@daum.net>
2021-01-11 18:24:17 +0900

    refactor: 유효성 검사를 분리하고 함수를 정리
    rename:
      ALLOWED_NUMBER_COUNT -> REQUIRED_SIZE
      areAllDistinctNumbers -> isDuplicated
    
    add:
      getDuplicatedElements
      getNumberOfEqualElements
    
    remove:
      toStream

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -2,50 +2,76 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class LottoNumbers {
-    static final int ALLOWED_NUMBER_COUNT = 6;
+    public static final int REQUIRED_SIZE = 6;
 
-    private final List<LottoNumber> lottoNumbers;
+    private final List<LottoNumber> numbers;
 
-    public LottoNumbers(List<LottoNumber> lottoNumbers) {
-        if (areInvalidLottoNumbers(lottoNumbers)) {
-            String message = String.format("Size of lottoNumbers should be %d.",
-                    ALLOWED_NUMBER_COUNT);
+    public LottoNumbers(List<LottoNumber> numbers) {
+        validate(numbers);
+        this.numbers = Collections.unmodifiableList(numbers);
+    }
+
+    private void validate(List<LottoNumber> numbers) {
+        if (isInvalidSize(numbers)) {
+            String message = String.format("%d개의 로또 번호가 주어져야 합니다.", REQUIRED_SIZE);
             throw new IllegalArgumentException(message);
         }
 
-        this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
+        if (isDuplicated(numbers)) {
+            String elements = getDuplicatedElements(numbers).stream()
+                    .map(LottoNumber::toString)
+                    .collect(Collectors.joining(", "));
+            String message = String.format("%s -> 서로 다른 %d개의 숫자여야 합니다.",
+                    elements, REQUIRED_SIZE);
+            throw new IllegalArgumentException(message);
+        }
     }
 
-    public Stream<LottoNumber> toStream() {
-        return lottoNumbers.stream();
+    private boolean isInvalidSize(List<LottoNumber> numbers) {
+        return numbers.size() != REQUIRED_SIZE;
     }
 
-    private boolean areInvalidLottoNumbers(List<LottoNumber> lottoNumbers) {
-        return isInvalidSize(lottoNumbers) || !areAllDistinctNumbers(lottoNumbers);
-    }
-
-    private boolean isInvalidSize(List<LottoNumber> lottoNumbers) {
-        return lottoNumbers.size() != ALLOWED_NUMBER_COUNT;
-    }
-
-    private boolean areAllDistinctNumbers(List<LottoNumber> lottoNumbers) {
-        return lottoNumbers.stream()
+    private boolean isDuplicated(List<LottoNumber> numbers) {
+        return numbers.stream()
                 .distinct()
-                .count() == lottoNumbers.size();
+                .count() != numbers.size();
     }
 
-    public boolean contains(LottoNumber lottonumber) {
-        return lottoNumbers.contains(lottonumber);
+    private List<LottoNumber> getDuplicatedElements(List<LottoNumber> numbers) {
+        return numbers.stream()
+                .distinct()
+                .filter(number ->
+                        numbers.indexOf(number) != numbers.lastIndexOf(number))
+                .collect(Collectors.toList());
+    }
+
+    public Count getNumberOfEqualElements(LottoNumbers lottoNumbers) {
+        return new Count(
+                (int) numbers.stream()
+                        .filter(lottoNumbers.numbers::contains)
+                        .count());
+    }
+
+    @Override
+    public String toString() {
+        return "[" + numbers.stream()
+                .map(LottoNumber::toString)
+                .collect(Collectors.joining(", ")) + "]";
+    }
+
+    public boolean contains(LottoNumber lottoNumber) {
+        return numbers.contains(lottoNumber);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof LottoNumbers) {
-            LottoNumbers numbers = (LottoNumbers) obj;
-            return this.lottoNumbers.containsAll(numbers.lottoNumbers);
+            LottoNumbers lottoNumbers = (LottoNumbers) obj;
+            return this.numbers.containsAll(lottoNumbers.numbers)
+                    && this.numbers.size() == lottoNumbers.numbers.size();
         }
         return false;
     }
