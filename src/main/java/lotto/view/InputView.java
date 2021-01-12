93b364b7059bdb4ff3b39b1839f02dba667ea00c
Sangyeol Kim <syeol.kim@daum.net>
2021-01-12 17:52:27 +0900

    feat: 수동으로 구매할 로또 정보 입력 함수 추가
    getNumberOfManualLotto:
      수동으로 구매할 로또의 수를 입력받고, 그 유효성을 검증함. 만약, 결과가 유효하지 않다면 재입력을 요구함.
    getManualLottoList:
      수동으로 구매할 로또 번호를 입력받아 리스트로 반환함.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,14 @@
 package lotto.view;
 
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoNumbers;
-import lotto.domain.LottoTicketIssuer;
-import lotto.domain.Price;
+import lotto.domain.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import static lotto.domain.LottoTicketIssuer.LOTTO_PRICE;
 
 public final class InputView {
     public static Price getInputPrice() {
@@ -23,6 +24,53 @@ public final class InputView {
             }
         }
         return inputPrice;
+    }
+
+    public static Count getNumberOfManualLotto(Price inputPrice) {
+        Count numberOfManualLotto = null;
+        while (true) {
+            try {
+                System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+                numberOfManualLotto = new Count(getInt());
+                validateNumberOfManualLotto(inputPrice, numberOfManualLotto);
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return numberOfManualLotto;
+    }
+
+    private static void validateNumberOfManualLotto(Price inputPrice, Count numberOfManualLotto) {
+        Count maxNumberOfLotto = new Count(inputPrice.divide(LOTTO_PRICE).getPrice());
+        if (maxNumberOfLotto.getCount() < numberOfManualLotto.getCount()) {
+            String message = String.format("잔액이 부족합니다. 최대 %d개 구매 가능합니다.", maxNumberOfLotto.getCount());
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static List<LottoNumbers> getManualLottoList(Count numberOfManualLotto) {
+        if(numberOfManualLotto.getCount() == 0) {
+            return null;
+        }
+
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<LottoNumbers> manualLottoList = new ArrayList<>();
+        while (manualLottoList.size() < numberOfManualLotto.getCount()) {
+            try {
+                LottoNumbers lottoNumbers = new LottoNumbers(
+                        Arrays.stream(getStringArray())
+                                .map(String::trim)
+                                .map(Integer::parseInt)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                );
+                manualLottoList.add(lottoNumbers);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return manualLottoList;
     }
 
     public static LottoNumbers getLuckyNumbers() {
@@ -61,7 +109,7 @@ public final class InputView {
     }
 
     private static void validateInputPrice(Price inputPrice) {
-        if (inputPrice.getPrice() < LottoTicketIssuer.TICKET_PRICE.getPrice()) {
+        if (inputPrice.getPrice() < LOTTO_PRICE.getPrice()) {
             throw new IllegalArgumentException(inputPrice.getPrice() + " -> 최소 1000원 이상 입력해 주세요.");
         }
     }
