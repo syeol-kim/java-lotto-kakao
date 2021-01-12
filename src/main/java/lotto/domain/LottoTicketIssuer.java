fc28f4de45266118706ca4e78ab2cb889e6df7f0
Sangyeol Kim <syeol.kim@daum.net>
2021-01-12 17:48:40 +0900

    refactor: 티켓 발급 함수 수정
    기존에는 구입금액으로 살 수 있는 최대치의 로또 번호를 자동 생성함.
    이제는 최대치에서 수동으로 입력 받은 로또 번호의 개수를 뺀 만큼 로또 번호를 자동 생성하고, 수동 로또 번호와 자동 로또 번호를 하나의 티켓에 담아서 반환함.

--- src/main/java/lotto/domain/LottoTicketIssuer.java
+++ src/main/java/lotto/domain/LottoTicketIssuer.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public final class LottoTicketIssuer {
-    public static final Price TICKET_PRICE = new Price(1000);
+    public static final Price LOTTO_PRICE = new Price(1000);
     private static final List<LottoNumber> numberList = IntStream.rangeClosed(
             LottoNumber.LOWER_BOUND, LottoNumber.UPPER_BOUND)
             .mapToObj(LottoNumber::new)
@@ -15,14 +16,19 @@ public final class LottoTicketIssuer {
     private LottoTicketIssuer() {
     }
 
-    public static LottoTicket issue(Price inputPrice) {
-        List<LottoNumbers> numbersList =
-                IntStream.range(0,
-                        inputPrice.divide(TICKET_PRICE)
-                                .getPrice()
+    public static LottoTicket issue(Price inputPrice, List<LottoNumbers> numbersList) {
+        validate(inputPrice, numbersList);
+        if (numbersList == null) {
+            numbersList = new ArrayList<>();
+        }
+
+        numbersList.addAll(
+                IntStream.range(0, inputPrice.divide(LOTTO_PRICE)
+                        .getPrice() - numbersList.size()
                 )
                         .mapToObj(x -> createLottoNumbersRandomly())
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toList())
+        );
         return new LottoTicket(numbersList);
     }
 
@@ -32,5 +38,22 @@ public final class LottoTicketIssuer {
                 .limit(LottoNumbers.REQUIRED_SIZE)
                 .collect(Collectors.toList());
         return new LottoNumbers(numbers);
+    }
+
+    private static void validate(Price inputPrice, List<LottoNumbers> manualLottoList) {
+        if (isNotEnoughPrice(inputPrice, manualLottoList)) {
+            Price difference = new Price(
+                    manualLottoList.size() * LOTTO_PRICE.getPrice() - inputPrice.getPrice()
+            );
+            throw new IllegalArgumentException("-" + difference.getPrice() + "원 -> 금액이 부족합니다.");
+        }
+    }
+
+    private static boolean isNotEnoughPrice(Price inputPrice, List<LottoNumbers> manualLottoList) {
+        return !isEmptyList(manualLottoList) && inputPrice.divide(LOTTO_PRICE).getPrice() < manualLottoList.size();
+    }
+
+    private static boolean isEmptyList(List<?> list) {
+        return list == null || list.isEmpty();
     }
 }
--- src/test/java/lotto/domain/LottoTicketIssuerTest.java
+++ src/test/java/lotto/domain/LottoTicketIssuerTest.java
@@ -1,26 +1,109 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoTicketIssuerTest {
     @ParameterizedTest
     @ValueSource(ints = {1000, 1999, 2000, 2001})
-    public void Should_SucceedInIssuance(int price) {
+    public void Should_SucceedInIssuance_WithoutManualLottoList(int price) {
         Price inputPrice = new Price(price);
-        LottoTicket ticket = LottoTicketIssuer.issue(inputPrice);
+        LottoTicket ticket = LottoTicketIssuer.issue(inputPrice, null);
         assertThat(ticket)
                 .isInstanceOf(LottoTicket.class);
+        assertThat(ticket.getLottoNumbersList().size())
+                .isEqualTo(inputPrice.getPrice() / 1000);  // inputPrice / LOTTO_PRICE
+    }
+
+    @Test
+    public void Should_SucceedInIssuance_WithManualLottoList() {
+        Price inputPrice = new Price(5500);
+        LottoNumbers first = new LottoNumbers(
+                Stream.of(1, 2, 3, 4, 5, 6)
+                        .map(LottoNumber::new)
+                        .collect(Collectors.toList())
+        );
+        LottoNumbers second = new LottoNumbers(
+                Stream.of(2, 3, 4, 5, 6, 7)
+                        .map(LottoNumber::new)
+                        .collect(Collectors.toList())
+        );
+
+        List<LottoNumbers> manualLottoList = Arrays.asList(
+                first, second);
+        LottoTicket ticket = LottoTicketIssuer.issue(inputPrice, manualLottoList);
+
+        assertThat(ticket)
+                .isInstanceOf(LottoTicket.class);
+        assertThat(ticket.getLottoNumbersList().size())
+                .isEqualTo(5);  // inputPrice / LOTTO_PRICE
+        assertThat(ticket.getLottoNumbersList().get(0))
+                .isEqualTo(first);
+        assertThat(ticket.getLottoNumbersList().get(1))
+                .isEqualTo(second);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 100, 500, 999})
-    public void Should_FailToIssue_InputPriceLessThanThousand(int price) {
+    public void Should_FailToIssue_InputPriceLessThanThousand_WithoutManualLottoList(int price) {
         Price inputPrice = new Price(price);
-        assertThatThrownBy(() -> LottoTicketIssuer.issue(inputPrice))
+        assertThatThrownBy(() -> LottoTicketIssuer.issue(inputPrice, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("티켓은 최소 1개 이상의 로또 번호들을 가져야 합니다.");
+    }
+
+    @Test
+    public void Should_FailToIssue_InputPriceLessThanThousand_WithManualLottoList() {
+        Price inputPrice = new Price(500);
+        List<LottoNumbers> manualLottoList = Arrays.asList(
+                new LottoNumbers(
+                        Stream.of(1, 2, 3, 4, 5, 6)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                ),
+                new LottoNumbers(
+                        Stream.of(2, 3, 4, 5, 6, 7)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                )
+        );
+
+        assertThatThrownBy(() -> LottoTicketIssuer.issue(inputPrice, manualLottoList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("-1500원 -> 금액이 부족합니다.");
+    }
+
+    @Test
+    public void Should_FailToIssue_InputPriceIsNotEnough() {
+        Price inputPrice = new Price(2500);
+        List<LottoNumbers> manualLottoList = Arrays.asList(
+                new LottoNumbers(
+                        Stream.of(1, 2, 3, 4, 5, 6)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                ),
+                new LottoNumbers(
+                        Stream.of(2, 3, 4, 5, 6, 7)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                ),
+                new LottoNumbers(
+                        Stream.of(3, 4, 5, 6, 7, 8)
+                                .map(LottoNumber::new)
+                                .collect(Collectors.toList())
+                )
+        );
+
+        assertThatThrownBy(() -> LottoTicketIssuer.issue(inputPrice, manualLottoList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("-500원 -> 금액이 부족합니다.");
     }
 }
